b914a8937904e9b1f2f2a086f7e1f0f3096d8af9
Jake <jakepalmer@Jakes-MacBook-Air-2.local>
2021-12-27 12:31:18 +0000

    test commit

--- SignalRChat/Program.cs
+++ SignalRChat/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// jake
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");

71bb0d9f34b4f97617a96bd2640b74eee95d0267
Jake <jakepalmer@Jakes-MacBook-Air-2.local>
2021-12-27 12:49:04 +0000

    Add chathub

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat.Hubs
+{
+	public class ChatHub : Hub
+	{
+		public ChatHub()
+		{
+		}
+		public async Task SendMessage(string user, string message)
+		{
+			await Clients.All.SendAsync("ReceiveMessage", user, message);
+		}
+
+	}
+}
+

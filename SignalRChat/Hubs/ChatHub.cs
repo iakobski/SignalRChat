ac27c09efe945ae7aefae65ed72d816e52883c18
Jake <jakepalmer@Jakes-MacBook-Air-2.local>
2021-12-27 19:28:02 +0000

    calculation demo

--- SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/Hubs/ChatHub.cs
@@ -7,12 +7,35 @@ namespace SignalRChat.Hubs
 	{
 		public ChatHub()
 		{
+
 		}
+		// represent the calculation as an int
+		private static Dictionary<string, int> Calculation = new Dictionary<string, int>();
+
 		public async Task SendMessage(string user, string message)
 		{
 			await Clients.All.SendAsync("ReceiveMessage", user, message);
 		}
 
+		public async Task StartCalculation(string user, string message)
+		{
+			var id = Context.ConnectionId;
+			if (!Calculation.TryAdd(id, 1))
+				Calculation[id] = 1;
+			await Clients.Caller.SendAsync("ReceiveMessage", "system", "started calculation");
+			Thread.Sleep(2000);
+			await Clients.Caller.SendAsync("NeedMoreData", "system", "mid calculation");
+		}
+		public async Task ContinueCalculation(string data)
+		{
+			var id = Context.ConnectionId;
+			await Clients.Caller.SendAsync("ReceiveMessage", "system", "continuing calculation");
+			Thread.Sleep(2000);
+			if (Calculation[id]++ < 4)
+				await Clients.Caller.SendAsync("NeedMoreData", "system", "mid calculation");
+			else 
+				await Clients.Caller.SendAsync("FinishedCalculation", "result");
+		}
 	}
 }
 
--- SignalRChat/Program.cs
+++ SignalRChat/Program.cs
@@ -1,27 +1,32 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using SignalRChat.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+builder.Services.AddLogging();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-// jake
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapRazorPages();
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
 
